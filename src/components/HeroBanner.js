96082e90d405129ca3eb4b7bcc6730f1f101fd50
Ben Chau <78128664+ben842002@users.noreply.github.com>
2022-07-04 09:58:30 -0400

    finish hero component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, Button } from '@mui/material';
+import HeroBannerImage from '../assets/images/banner.png';
+import zIndex from '@mui/material/styles/zIndex';
 
 const HeroBanner = () => {
   return (
@@ -11,15 +13,31 @@ const HeroBanner = () => {
     position="relative" p="20px"
     >
         <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-            Fitness Club
+            Going The Distance Club
         </Typography>
 
-        <Typography fontWeight="700" sx={{ fontSize: { lg: '44px', xs: '40px' }}}>
-            Sweat, Smile, <br /> and Repeat
+        <Typography fontWeight="700" sx={{ fontSize: { lg: '44px', xs: '40px' }}} mb="23px" mt="30px">
+            Wake Up, Get After It, <br /> and Repeat
         </Typography>
 
-        <Typography fontSize="22px" lineHeight="35px">
+        <Typography fontSize="22px" lineHeight="35px" mb={4}>
             Check out the most effective exercises
+        </Typography>
+
+        <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: '#ff2625', padding: '10px'}}>Explore Exercises</Button>
+        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" style={{zIndex:1}}/>
+
+        <Typography 
+            fontWeight={600} 
+            color="#ff2625" 
+            fontSize="200px"
+            style={{zIndex:0}}
+            sx={{ 
+                opacity: 0.1, 
+                display: { lg: 'block', xs: 'none' },
+            }}
+        >
+            Exercise
         </Typography>
     </Box>
   )
