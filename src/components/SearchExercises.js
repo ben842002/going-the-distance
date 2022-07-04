656cc00116a17422fd1602894185c5bdafb417d4
Ben Chau <78128664+ben842002@users.noreply.github.com>
2022-07-04 19:31:15 -0400

    implement Horiz. Scrollbar and Body Part Component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, setBodyPart, bodyPart }) => {
 
   const [search, setSearch] = useState("");
+  const [bodyParts, setBodyParts] = useState([]);
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      // encompass multiple ways user searches for something. Object attribute names are given in the fetched data
+      const searchedExercises = exercisesData.filter((exercise) => exercise.name.toLowerCase().includes(search) 
+        || exercise.target.toLowerCase().includes(search) 
+        || exercise.bodyPart.toLowerCase().includes(search) 
+        || exercise.equipment.toLowerCase().includes(search) 
+      );
+      
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  }
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
@@ -39,9 +66,15 @@ const SearchExercises = () => {
           position: 'absolute',
           right: '0'
           }}
+
+          onClick={handleSearch}
         >
           Search
         </Button>
+      </Box>
+
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   )
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,11 @@
 // Utility functions
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
 
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
