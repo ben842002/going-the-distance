a9b4b92c9cfd511dd8f0bcae8180f0f98f83b21a
Ben Chau <78128664+ben842002@users.noreply.github.com>
2022-07-07 21:01:02 -0400

    begin ExerciseDetail Component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { fetchData, exerciseOptions } from '../utils/fetchData';
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'; // for the id of the exercise we are on
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData} from '../utils/fetchData';
+
+import Detail from '../components/ExerciseDetail/Detail';
+import ExerciseVideos from '../components/ExerciseDetail/ExerciseVideos';
+import SimilarExercises from '../components/ExerciseDetail/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({
+
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      // access APIs
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+
+    fetchExercisesData();
+  }, []);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
